c17ce50cbf60460b00fd252a9fa9af5638d72fd6
TonygCSUN <TonygCSUN@users.noreply.github.com>
2021-02-02 15:15:34 -0800

    Add files via upload

---
+++ scripts/powershellscript_test.ps1
@@ -0,0 +1,5 @@
+
+# random/specific powershell command(s) to execute
+# Restart-Service -Name Spooler -Force
+
+Stop-Service -Name Spooler -Force
---
+++ scripts/sccm_client_cache_cleanup.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module ActiveDirectory
+
+$servers = @()
+$servers += Get-ADGroupMember -Identity "SCCM-IT-Thursday-Servers" | foreach { $_.Name }
+$servers += Get-ADGroupMember -Identity "SCCM-IT-Production-Servers" | foreach { $_.Name }
+$servers += Get-ADGroupMember -Identity "SCCM-IT-Test-Servers" | foreach { $_.Name }
+
+Invoke-Command -ComputerName $servers -ScriptBlock {
+##Clean-up SCCM client cache
+Write-Host "Cleaning up the SCCM update cache folder on $env:computername" -ForegroundColor Green
+
+$resman = new-object -com "UIResource.UIResourceMgr"
+$cacheInfo = $resman.GetCacheInfo()
+$cacheinfo.GetCacheElements() | foreach {$cacheInfo.DeleteCacheElement($_.CacheElementID)}
+}
